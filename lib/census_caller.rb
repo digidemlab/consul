8f791cf9d0cab17a978d6487fc045553ec8ea4d6
Pierre Mesure <pierre.mesure@gmail.com>
2019-08-16 11:26:43 +0200

    Removing PersonnummerApi and adding a fully permissive API which lets you use any ID number

--- lib/census_caller.rb
+++ lib/census_caller.rb
@@ -1,8 +1,6 @@
 class CensusCaller
 
   def call(document_type, document_number)
-    #response = CensusApi.new.call(document_type, document_number)
-    #response = LocalCensus.new.call(document_type, document_number) unless response.valid?
-    PersonnummerApi.new.call(document_type, document_number)
+    NoBorderApi.new.call(document_type, document_number)
   end
 end
