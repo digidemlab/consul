5ce444ca54aa6f404f5e44f6ab0c47fef1a72df0
Pierre Mesure <pierre@mesu.re>
2020-03-30 14:58:48 +0200

    Fix config with paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,9 +101,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
 
-# Paperclip settings to store images and documents on S3
+  # Paperclip settings to store images and documents on S3
   config.paperclip_defaults = {
     storage: :s3,
     preserve_files: true,
